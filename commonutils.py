caffb82242fb58e044624d67b786598473937fb3
Sri Harsha Mittapalli <53350799+SriHarshaMittapalli@users.noreply.github.com>
2023-04-22 19:27:53 -0400

    Update commonutils.py
    Optimize code

--- commonutils.py
+++ commonutils.py
@@ -15,42 +15,17 @@ def tile_components(tile_component_list):
 
 # Define a function to fetch the latest data from ThingSpeak
 def get_latest_data():
-    url = "https://api.thingspeak.com/channels/2097821/feeds.json"
+    url = "https://api.thingspeak.com/channels/2085717/feeds.json"
     response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        return pd.DataFrame(data["feeds"])
-    else:
-        return pd.DataFrame()
+    if response.status_code == 200: return pd.DataFrame(response.json()["feeds"])
+    else: return pd.DataFrame()
 
 def graph_component(data,type,title):
     data["Date"] = pd.to_datetime(data["created_at"])
     data[title] = data[type].astype(float)
-    
     # define dynamic y-axis scale
     y_scale = alt.Scale(domain=(data[title].min() - 1, data[title].max() + 1))
-
-    line = (
-        alt.Chart(data[['Date', title]])
-        .mark_line(color='blue')  # set line color
-        .encode(
-            alt.X("Date:T",title="Time"),
-            alt.Y(title + ":Q",title=title, scale=y_scale),
-            tooltip=["Date:T", title + ":Q"],
-        )
-        .properties(title=title + " vs Time")
-    )
-
-    points = (
-        alt.Chart(data[['Date', title]])
-        .mark_point(color='red',size=30)  # set point color
-        .encode(
-            alt.X("Date:T",title="Time"),
-            alt.Y(title + ":Q",title=title, scale=y_scale),
-            tooltip=["Date:T", title + ":Q"],
-        )
-    )
-
+    line = (alt.Chart(data[['Date', title]]).mark_line(color='blue').encode(alt.X("Date:T",title="Time"), alt.Y(title + ":Q",title=title, scale=y_scale), tooltip=["Date:T", title + ":Q"]).properties(title=title + " vs Time"))
+    points = (alt.Chart(data[['Date', title]]).mark_point(color='red',size=30).encode(alt.X("Date:T",title="Time"), alt.Y(title + ":Q",title=title, scale=y_scale), tooltip=["Date:T", title + ":Q"]))
     chart = line + points
-
     st.altair_chart(chart,theme="streamlit",use_container_width=True)
