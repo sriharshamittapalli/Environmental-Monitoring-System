ba2eee0a58b730c16da89ecb0692bde9d885ce8b
Sri Harsha Mittapalli <53350799+SriHarshaMittapalli@users.noreply.github.com>
2023-05-07 12:36:17 -0400

    Update commonutils.py

--- commonutils.py
+++ commonutils.py
@@ -1,4 +1,3 @@
-import altair as alt # This library used for graphs, referred from https://altair-viz.github.io/
 import pandas as pd # This library is used for data, referred from https://pandas.pydata.org/
 import requests # This library is used to get data from api, referred from https://pypi.org/project/requests/
 import streamlit as st # This is the main library, referred from https://streamlit.io/
@@ -22,14 +21,3 @@ def get_latest_data():
         return pd.DataFrame(response.json()["feeds"]) # using the dataframe function from requests library.
     else:
         return pd.DataFrame()
-
-def line_graph_component(data,type,title):
-    data["Date"] = pd.to_datetime(data["created_at"])
-    data[title] = data[type].astype(float)
-    # define dynamic y-axis scale
-    y_scale = alt.Scale(domain=(data[title].min() - 1, data[title].max() + 1))
-    line = (alt.Chart(data[['Date', title]]).mark_line(color='blue').encode(alt.X("Date:T",title="Time"), alt.Y(title + ":Q",title=title, scale=y_scale), tooltip=["Date:T", title + ":Q"]).properties(title=title + " vs Time"))
-    points = (alt.Chart(data[['Date', title]]).mark_point(color='red',size=30).encode(alt.X("Date:T",title="Time"), alt.Y(title + ":Q",title=title, scale=y_scale), tooltip=["Date:T", title + ":Q"]))
-    chart = line + points
-    st.altair_chart(chart,theme="streamlit",use_container_width=True)
-
