796d64ace9c4f2c130fd56fcc6a85b5c4deaf01e
Sri Harsha Mittapalli <53350799+SriHarshaMittapalli@users.noreply.github.com>
2023-05-07 16:40:04 -0400

    Update linegraphcomponent.py

--- components/linegraphcomponent.py
+++ components/linegraphcomponent.py
@@ -3,11 +3,57 @@ import pandas as pd # This library is used for data, referred from https://panda
 import streamlit as st # This is the main library, referred from https://streamlit.io/
 
 def line_graph_component(data,type,title):
-    data["Date"] = pd.to_datetime(data["created_at"])
+    # Learned how to design line graphs and points from https://altair-viz.github.io/
+    data["Date"] = pd.to_datetime(data["created_at"]) # Using the to_datetime function from pandas.
     data[title] = data[type].astype(float)
-    # define dynamic y-axis scale
-    y_scale = alt.Scale(domain=(data[title].min() - 1, data[title].max() + 1))
-    line = (alt.Chart(data[['Date', title]]).mark_line(color='blue').encode(alt.X("Date:T",title="Time"), alt.Y(title + ":Q",title=title, scale=y_scale), tooltip=["Date:T", title + ":Q"]).properties(title=title + " vs Time"))
-    points = (alt.Chart(data[['Date', title]]).mark_point(color='red',size=30).encode(alt.X("Date:T",title="Time"), alt.Y(title + ":Q",title=title, scale=y_scale), tooltip=["Date:T", title + ":Q"]))
-    chart = line + points
-    st.altair_chart(chart,theme="streamlit",use_container_width=True)
+    # Using Scale function from altair library.
+    y_scale = alt.Scale(
+                        domain = (
+                                 data[title].min() - 1,
+                                 data[title].max() + 1
+                                 )
+                        )
+    line = (
+           alt.Chart(data[["Date", title]]) # Using Chart function from altair library.
+           .mark_line(color = "blue") # Marking the line color as blue.
+           .encode(
+                 alt.X(
+                      "Date:T", # Formating date, also learned how to do these operations from https://altair-viz.github.io/
+                      title = "Time"
+                 ),
+                 alt.Y(
+                      title + ":Q", # Formating title, also learned how to do these operations from https://altair-viz.github.io/
+                      title = title,
+                      scale = y_scale
+                 ),
+                 tooltip=["Date:T", title + ":Q"])
+           .properties(
+                      title = title + " vs Time"
+                      )
+            )
+    
+    points = (
+            alt.Chart(data[["Date", title]]) # Using Chart function from altair library.
+            .mark_point(
+                       color = "red", # Marking the points color as red
+                       size = 30
+                       )
+            .encode(
+                   alt.X(
+                        "Date:T",
+                        title = "Time"
+                        ),
+                   alt.Y(
+                        title + ":Q",
+                        title = title,
+                        scale = y_scale
+                        ),
+                   tooltip=["Date:T", title + ":Q"]
+                   )
+            )
+    # Using altair_chart function from streamlit library.
+    st.altair_chart(
+                    line + points,
+                    theme = "streamlit",
+                    use_container_width = True
+                    )
