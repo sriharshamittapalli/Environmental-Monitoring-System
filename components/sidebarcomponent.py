8e3e8e21bf9fcefc980babe433fe90eaa1d65997
Sri Harsha Mittapalli <53350799+SriHarshaMittapalli@users.noreply.github.com>
2023-05-07 14:18:55 -0400

    Update sidebarcomponent.py

--- components/sidebarcomponent.py
+++ components/sidebarcomponent.py
@@ -1,11 +1,16 @@
-import streamlit as st # This is the main library, referred from https://streamlit.io/
+# This is the main library, referred from https://streamlit.io/
+import streamlit as st
 
 def sidebar_component(csu_image, project_title, download_dataset_text, download_dataset_hyperlink):
-    # using the image function from streamlit library.
-    st.sidebar.image(csu_image)
-    # using the title function from streamlit library.
-    st.sidebar.title(project_title)
-    # using the write function from streamlit library.
-    st.sidebar.write(download_dataset_text)
-    st.sidebar.write(download_dataset_hyperlink)
+    if csu_image:
+        # using the image function from streamlit library.
+        st.sidebar.image(csu_image)
+    if project_title:
+        # using the title function from streamlit library.
+        st.sidebar.title(project_title)
+    if download_dataset_text:
+        # using the write function from streamlit library.
+        st.sidebar.write(download_dataset_text)
+    if download_dataset_hyperlink:
+        st.sidebar.write(download_dataset_hyperlink)
     return
