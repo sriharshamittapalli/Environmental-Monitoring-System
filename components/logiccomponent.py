7e626ca385aa2984fc0e676b11a1a4626e7e22b6
Sri Harsha Mittapalli <53350799+SriHarshaMittapalli@users.noreply.github.com>
2023-05-07 12:36:00 -0400

    Update logiccomponent.py

--- components/logiccomponent.py
+++ components/logiccomponent.py
@@ -2,6 +2,7 @@ from commonutils import * # Created all important functions in commonutils which
 from datetime import datetime # This is used for timestamp conversions. referred from https://docs.python.org/3/library/datetime.html
 import pytz # This is used for timezone conversions. referred from https://pypi.org/project/pytz/
 from components import metriccomponent
+from components import linegraphcomponent
 
 def logic_component(message_component_data, tile_component_data, tabs_component_data, tab1_graph_component, tab2_graph_component):
     # Get the latest data from ThingSpeak
@@ -23,20 +24,20 @@ def logic_component(message_component_data, tile_component_data, tabs_component_
                         tile_component_data[3])
         with tabs_component_data[0]:
             with tab1_graph_component[0]:
-                line_graph_component(data_csv[["created_at", "field1"]].copy(deep=True),
+                linegraphcomponent.line_graph_component(data_csv[["created_at", "field1"]].copy(deep=True),
                                     "field1",
                                     "Temperature")
             with tab1_graph_component[1]:
-                line_graph_component(data_csv[["created_at", "field2"]].copy(deep=True),
+                linegraphcomponent.line_graph_component(data_csv[["created_at", "field2"]].copy(deep=True),
                                     "field2",
                                     "Humidity")
         with tabs_component_data[1]:
             with tab2_graph_component[0]:
-                line_graph_component(data_csv[["created_at", "field3"]].copy(deep=True),
+                linegraphcomponent.line_graph_component(data_csv[["created_at", "field3"]].copy(deep=True),
                                     "field3",
                                     "Air Quality")
             with tab2_graph_component[1]:
-                line_graph_component(data_csv[["created_at", "field4"]].copy(deep=True),
+                linegraphcomponent.line_graph_component(data_csv[["created_at", "field4"]].copy(deep=True),
                                     "field4",
                                     "Smoke")
         if int(float(data_csv["field4"].iloc[-1])) > 400: message_component_data[1].warning(datetime.fromisoformat(data_csv["created_at"].iloc[-1][:-1]).replace(tzinfo=pytz.utc).astimezone(pytz.timezone('US/Eastern')).strftime("Smoke detected at **%B %d, %Y** at **%I:%M:%S %p** EDT."), icon="⚠️")
