9a7fcee6ba6ba793e4ab2c25261ba185672bf50a
Sri Harsha Mittapalli <53350799+SriHarshaMittapalli@users.noreply.github.com>
2023-05-07 14:20:05 -0400

    Update messagecomponent.py

--- components/messagecomponent.py
+++ components/messagecomponent.py
@@ -1,7 +1,11 @@
+# Implemented all important functions in commonutils.
 from commonutils import *
 
 def message_component(messages_list):
-    messages_list[0], messages_list[1] = columns(len(messages_list))
-    messages_list[0] = empty(messages_list[0])
-    messages_list[1] = empty(messages_list[1])
+    if messages_list:
+        if messages_list[0]:
+            if messages_list[1]:
+                messages_list[0], messages_list[1] = columns(len(messages_list))
+                messages_list[0] = empty(messages_list[0])
+                messages_list[1] = empty(messages_list[1])
     return messages_list[0], messages_list[1]
