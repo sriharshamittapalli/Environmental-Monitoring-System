318fd36faaebbbe1a0177231ebdb79235e1e200e
Sri Harsha Mittapalli <53350799+SriHarshaMittapalli@users.noreply.github.com>
2023-05-07 14:17:04 -0400

    Update titlecomponent.py

--- components/titlecomponent.py
+++ components/titlecomponent.py
@@ -3,7 +3,10 @@ import streamlit as st
 
 def title_component(title_of_page, layout_of_page, sidebar_state):
     # using the set_page_config function from streamlit library.
-    st.set_page_config(page_title = title_of_page,
-                       layout = layout_of_page,
-                       initial_sidebar_state = sidebar_state)
+    if title_of_page:
+        if layout_of_page:
+            if sidebar_state:
+                st.set_page_config(page_title = title_of_page,
+                                layout = layout_of_page,
+                                initial_sidebar_state = sidebar_state)
     return
