9704f12a65284582bea23184c6561ff3234779fc
Sri Harsha Mittapalli <53350799+SriHarshaMittapalli@users.noreply.github.com>
2023-05-06 23:57:21 -0400

    Create titlecomponent.py

---
+++ components/titlecomponent.py
@@ -0,0 +1,9 @@
+# This is the main library, referred from https://streamlit.io/
+import streamlit as st
+
+def title_component(title_of_page, layout_of_page, sidebar_state):
+    # using the set_page_config function from streamlit library.
+    st.set_page_config(page_title = title_of_page,
+                       layout = layout_of_page,
+                       initial_sidebar_state = sidebar_state)
+    return
