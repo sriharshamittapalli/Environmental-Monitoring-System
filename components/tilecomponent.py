6c239c46c84bddac20d26a22c128626d744445d5
Sri Harsha Mittapalli <53350799+SriHarshaMittapalli@users.noreply.github.com>
2023-05-07 14:17:32 -0400

    Update tilecomponent.py

--- components/tilecomponent.py
+++ components/tilecomponent.py
@@ -1,9 +1,15 @@
+# Implemented all important functions in commonutils.
 from commonutils import *
 
 def tile_component(tiles_list):
-    tiles_list[0],tiles_list[1],tiles_list[2],tiles_list[3] = columns(len(tiles_list))
-    tiles_list[0] = empty(tiles_list[0])
-    tiles_list[1] = empty(tiles_list[1])
-    tiles_list[2] = empty(tiles_list[2])
-    tiles_list[3] = empty(tiles_list[3])
+    if tiles_list:
+        if tiles_list[0]:
+            if tiles_list[1]:
+                if tiles_list[2]:
+                    if tiles_list[3]:
+                        tiles_list[0],tiles_list[1],tiles_list[2],tiles_list[3] = columns(len(tiles_list))
+                        tiles_list[0] = empty(tiles_list[0])
+                        tiles_list[1] = empty(tiles_list[1])
+                        tiles_list[2] = empty(tiles_list[2])
+                        tiles_list[3] = empty(tiles_list[3])
     return tiles_list[0], tiles_list[1], tiles_list[2], tiles_list[3]
