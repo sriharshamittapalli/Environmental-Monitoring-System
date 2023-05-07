979a9c9c049353dabb9417b8335f08d4cb793f56
Sri Harsha Mittapalli <53350799+SriHarshaMittapalli@users.noreply.github.com>
2023-05-07 13:41:58 -0400

    Update commonutils.py

--- commonutils.py
+++ commonutils.py
@@ -10,14 +10,15 @@ def columns(number, tab = False):
     # using the columns function from streamlit library.
     if tab:
         return tab.columns(number)
-    return st.columns(number)
+    else:
+        return st.columns(number)
 
 # Define a function to fetch the latest data from ThingSpeak
 def get_latest_data():
     channel_feed = "https://api.thingspeak.com/channels/2097821/feeds.json"
     # using the get function from requests library.
-    response = requests.get(channel_feed)
-    if response.status_code == 200:
-        return pd.DataFrame(response.json()["feeds"]) # using the dataframe function from requests library.
+    json_response = requests.get(channel_feed)
+    if json_response.status_code == 200:
+        return pd.DataFrame(json_response.json()["feeds"]) # using the dataframe function from requests library.
     else:
         return pd.DataFrame()
