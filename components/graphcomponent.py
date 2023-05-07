b24527d94ed5a0deb32c1f7428853c3ce7e44ccf
Sri Harsha Mittapalli <53350799+SriHarshaMittapalli@users.noreply.github.com>
2023-05-07 14:20:37 -0400

    Update graphcomponent.py

--- components/graphcomponent.py
+++ components/graphcomponent.py
@@ -1,7 +1,12 @@
+# Implemented all important functions in commonutils.
 from commonutils import *
 
 def graph_component(graphs_list, tab):
-    graphs_list[0], graphs_list[1] = columns(len(graphs_list), tab)
-    graphs_list[0] = empty(graphs_list[0])
-    graphs_list[1] = empty(graphs_list[1])
+    if graphs_list:
+        if graphs_list[0]:
+            if graphs_list[1]:
+                if tab:
+                    graphs_list[0], graphs_list[1] = columns(len(graphs_list), tab)
+                    graphs_list[0] = empty(graphs_list[0])
+                    graphs_list[1] = empty(graphs_list[1])
     return graphs_list[0], graphs_list[1]
