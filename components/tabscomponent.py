dffe70c9e24fed3b281c593ccb8755f3ab557edd
Sri Harsha Mittapalli <53350799+SriHarshaMittapalli@users.noreply.github.com>
2023-05-07 14:18:21 -0400

    Update tabscomponent.py

--- components/tabscomponent.py
+++ components/tabscomponent.py
@@ -1,9 +1,14 @@
-import streamlit as st # This is the main library, referred from https://streamlit.io/
+# This is the main library, referred from https://streamlit.io/
+import streamlit as st
+# Implemented all important functions in commonutils.
 from commonutils import *
 
 def tabs_component(tabs_list):
-    # using the tabs function from streamlit library.
-    tabs_list[0], tabs_list[1] = st.tabs(tabs_list)
-    tabs_list[0] = empty(tabs_list[0])
-    tabs_list[1] = empty(tabs_list[1])
-    return tabs_list[0], tabs_list[1]
+    if tabs_list:
+        if tabs_list[0]:
+            if tabs_list[1]:
+                # using the tabs function from streamlit library.
+                tabs_list[0], tabs_list[1] = st.tabs(tabs_list)
+                tabs_list[0] = empty(tabs_list[0])
+                tabs_list[1] = empty(tabs_list[1])
+                return tabs_list[0], tabs_list[1]
